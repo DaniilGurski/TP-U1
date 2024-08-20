08ce20bc81481ca2efa2bf4dfcd85fc08d1763b6
DaniilGurski <daniil.gurski@elev.ga.ntig.se>
2024-08-20 10:48:37 +0200

    edit.

--- monthly_payment.py
+++ monthly_payment.py
@@ -9,7 +9,7 @@ def calculate_monthly_payment(principal, annual_rate, years):
     # räntan per period (t.ex. om räntan är 9% blir det r = 0.09 / 12)
     mounthly_rate = (annual_rate / 100) / 12
     month = years * 12
-    # monthly payment
+    # lön per månad
     return (principal * mounthly_rate * (1 + mounthly_rate) ** month) / ((1 + mounthly_rate) ** month - 1)
 
 
@@ -19,6 +19,3 @@ years = int(input("Löptid (år): "))
 
 payment = calculate_monthly_payment(principal, annual_rate, years)
 print(f"Din månatliga betalning är: {payment:.2f} kr")
-
-# 100000, 0.05, 30
-# return (principal * (mounthly_rate * (1 + mounthly_rate)**month)) / ((1 + mounthly_rate)**month - 1)
